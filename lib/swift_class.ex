a18e3112df69512068d6a6cc81cccdfdc2acb79d
Nduati Kuria <012nkuria@gmail.com>
2023-10-20 20:42:48 +0300

    WIP

--- lib/swift_class.ex
+++ lib/swift_class.ex
@@ -1,23 +1,22 @@
 defmodule SwiftClass do
-  @moduledoc false
-  import NimbleParsec
-  alias SwiftClass.Modifiers
-  alias SwiftClass.Tokens
-  import SwiftClass.Blocks
+  # @moduledoc false
+  # import NimbleParsec
+  # alias SwiftClass.Modifiers
+  # alias SwiftClass.Tokens
+  # import SwiftClass.Blocks
 
-  def parse(input, opts \\ []) do
-    case Modifiers.modifiers(input, opts) do
-      {:ok, [output], a, b, c, d} ->
-        {:ok, output, a, b, c, d}
+  # def parse(input, opts \\ []) do
+  #   case Modifiers.modifiers(input, opts) do
+  #     {:ok, [output], a, b, c, d} ->
+  #       {:ok, output, a, b, c, d}
 
-      other ->
-        other
-    end
-  end
+  #     other ->
+  #       other
+  #   end
+  # end
 
-  defparsec(
-    :parse_class_block,
-    Tokens.start()
-    |> repeat(parsec(:class_block))
-  )
+  # defparsec(
+  #   :parse_class_block,
+  #   repeat(Tokens.start(), parsec(:class_block))
+  # )
 end
